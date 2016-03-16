486cef2330abef49eb80d173c9a9f92929280fff
RayNi <rayni@raynidemacbook-pro.local>
2016-03-16 13:05:20 +0800

    Initial Commit

---
+++ FM1069/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FM1069
+//
+//  Created by RayNi on 16/3/16.
+//  Copyright © 2016年 RayNi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FM1069Tests/FM1069Tests.m
@@ -0,0 +1,39 @@
+//
+//  FM1069Tests.m
+//  FM1069Tests
+//
+//  Created by RayNi on 16/3/16.
+//  Copyright © 2016年 RayNi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FM1069Tests : XCTestCase
+
+@end
+
+@implementation FM1069Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
