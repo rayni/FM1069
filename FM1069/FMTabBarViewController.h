d09b68a6418f3a1b018da43c0b4cd8557ec7479b
RayNi <440137193@qq.com>
2016-03-17 21:57:10 +0800

    tabBar添加

---
+++ FM1069/FMTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMTabBarViewController.h
+//  FM1069
+//
+//  Created by RayNi on 16/3/17.
+//  Copyright © 2016年 RayNi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMTabBarViewController : UITabBarController
+
+@end
